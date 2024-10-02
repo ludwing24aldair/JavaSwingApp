1688ac167b10a36f586881938942438bcadf4552
ludwing24aldair <1480905@senati.pe>
2024-10-02 13:50:31 -0500

    Initial commit

---
+++ src/com/ludwing/pe/db/Conexion.java
@@ -0,0 +1,50 @@
+package com.ludwing.pe.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class Conexion {
+    private static Connection con = null;
+    private static String usuario = "system";
+    private static String clave = "123456";
+    private static String url = "jdbc:oracle:thin:@localhost:1521:xe";
+    
+    
+    public static Connection getConnection(){
+        try {
+            //Cargar el controlador JDBC
+            Class.forName("oracle.jdbc.OracleDriver");
+            
+            //Establecer la coneccion con la base de datos 
+            con = DriverManager.getConnection(url,usuario,clave);
+            con.setAutoCommit(false);
+            
+            System.out.println("Ludwing Aldair");
+            
+            if(con != null){
+                System.out.println("Conexion Exitosa");
+            }else{
+                System.out.println("Error: Conexion Fallida");
+            }
+            
+             
+        } catch(Exception e){
+            System.out.println("Error: "+e.getMessage());
+        }
+        return con;
+    };
+    
+    public void closeConnection(){
+        try {
+            con.close();
+        } catch (Exception e) {
+            System.out.println("Error al cerrar la conexion "+e.getMessage());
+        }
+    }
+    
+    public static void main(String[] args) {
+    //    Conexion con = new Conexion();
+        Conexion.getConnection();
+    }
+}
